3b1a953ad20d972524ab04ca22e82f3af3043421
Zeptosec <armandasmatkevicius@gmail.com>
2023-03-26 22:48:18 +0300

    Initial

---
+++ RentSystem/RentSystem.Core/DTOs/ItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using RentSystem.Core.Enums;
+
+namespace RentSystem.Core.DTOs
+{
+    public class ItemDTO
+    {
+        public Category Category { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public State State { get; set; }
+        public int AdvertId { get; set; }
+    }
+}
---
+++ RentSystem/RentSystem.Services/MappingProfiles/AdvertMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using RentSystem.Core.DTOs;
+using RentSystem.Core.Entities;
+
+namespace RentSystem.Services.MappingProfiles
+{
+    public class AdvertMappingProfile : Profile
+    {
+        public AdvertMappingProfile()
+        {
+            CreateMap<Advert, GetAdvertDTO>().ReverseMap();
+            CreateMap<Advert, AdvertDTO>()
+                .ForMember(m => m.DeliveryType,
+                       opt => opt.MapFrom(o => o.DeliveryType)).ReverseMap();
+        }
+    }
+}
